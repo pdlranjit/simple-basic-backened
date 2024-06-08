499ca5c0ed1296986cf2a8df09942766fc09a198
ranjit damase <ranjitdamase@gmail.com>
2024-06-08 15:40:16 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/none",(req,res)=>{
+    res.send("this is best")
+})
+app.get('/youtube',(req,res)=>{
+  res.send("this is very annoying")
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
